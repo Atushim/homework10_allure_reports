2356cab90cfa0ba8620ac8e87bffd5a88f37e4f1
daniil <daniil.mishuta@eltex.loc>
2022-09-20 15:46:46 +0700

    added allure

--- src/test/java/guru/qa/SelenideTest.java
+++ src/test/java/guru/qa/SelenideTest.java
@@ -1,9 +1,8 @@
 package guru.qa;
 
-import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.BeforeAll;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
-
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.withText;
@@ -14,7 +13,10 @@ import static org.openqa.selenium.By.linkText;
 public class SelenideTest {
 
     @Test
+
     public void TestIssueSearch(){
+        SelenideLogger.addListener("allure",new AllureSelenide());
+
         open("https://github.com/");
         $(".header-search-input").click();
         $(".header-search-input").setValue("Selenide");
